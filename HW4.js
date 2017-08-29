55d005e2bd37067efd13c7f06c6c90dfcf623cec
the2kb <the2kb@gmail.com>
2017-08-29 16:39:37 +0300

    makeBuffer
    https://learn.javascript.ru/task/stringbuffer

---
+++ HW4.js
@@ -0,0 +1,17 @@
+let makeBuffer = () {
+
+}
+
+let buffer = makeBuffer();
+
+function makeBuffer() { /* ваш код */ }
+
+var buffer = makeBuffer();
+
+// добавить значения к буферу
+buffer('Замыкания');
+buffer(' Использовать');
+buffer(' Нужно!');
+
+// получить текущее значение
+alert( buffer() ); // Замыкания Использовать Нужно!
